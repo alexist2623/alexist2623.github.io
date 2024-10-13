8efd39c5067560917121637272f8a9f52410aa52
ParkJeonghyun <49219392+alexist2623@users.noreply.github.com>
2024-10-13 21:44:53 +0900

    logging added

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 // Get the current page URL
 let currentLocation = location.href;
+console.log(currentLocation.replace(/\/index\.html(#.*)?$/, ''));
 
 // Select all navigation buttons (or links)
 const menuItems = document.querySelectorAll('.button');
 
 // Loop through the buttons and check if href matches the current URL
 menuItems.forEach(item => {
+    console.log(item.href.replace(/\/index\.html(#.*)?$/, ''));
     if (item.href.replace(/\/index\.html(#.*)?$/, '') === currentLocation.replace(/\/index\.html(#.*)?$/, '')) {
         item.classList.add('active');
     }
