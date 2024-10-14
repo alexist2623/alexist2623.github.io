6a0f2ce8c9d532654f61bb2d7d3730694fc9b673
ParkJeonghyun <49219392+alexist2623@users.noreply.github.com>
2024-10-14 13:23:39 +0900

    feature : Photos Update

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
 // Get the current page URL
-let currentLocation = location.href;
-console.log(currentLocation.replace(/\/index\.html(#.*)?$/, ''));
+let currentLocation = location.href.replace(/\/index\.html(#.*)?$/, '');
+if (currentLocation.endsWith("/")) {
+    currentLocation = currentLocation.slice(0, -1); // 마지막 슬래시 제거
+}
+console.log(currentLocation);
 
 // Select all navigation buttons (or links)
 const menuItems = document.querySelectorAll('.button');
 
 // Loop through the buttons and check if href matches the current URL
 menuItems.forEach(item => {
-    console.log(item.href.replace(/\/index\.html(#.*)?$/, ''));
-    if (item.href.replace(/\/index\.html(#.*)?$/, '') === currentLocation.replace(/\/index\.html(#.*)?$/, '')) {
+    let newitem = item.href.replace(/\/index\.html(#.*)?$/, '')
+    if (newitem.endsWith("/")) {
+        newitem = newitem.slice(0, -1); // 마지막 슬래시 제거
+    }
+    console.log(newitem);
+    if (newitem === currentLocation) {
         item.classList.add('active');
     }
 });
